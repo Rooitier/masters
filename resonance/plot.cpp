dd16ae6f58c05b3991433d4fee2f34f2fe28dfc3
Rooitier <rooitier@gmail.com>
2021-08-24 15:30:45 +0200

    24/08 Plotting RF ML results

--- resonance/plot.cpp
+++ resonance/plot.cpp
@@ -1,13 +1,13 @@
 // Macro used for plotting resonance data from two data files, can add more just need to duplicate sections of the code.
 // The data is combined from RIPL-3 and another unnamed data file which does not have an author but was provided by my supervisor.
 
-void plot(const char* file1){
+void plot(const char* file1, const char* file2){
 TCanvas *c1 = new TCanvas ("c1","Graph Draw Options", 200,10,600,400);
 c1->SetGrid();
-TMultiGraph *mg = new TMultiGraph();
+TMultiGraph *mg = new TMultiGraph("mg","Mass Number (A) versus Average Resonance Spacing D_{0} (keV) for neutron capture");
 
-TGraphErrors *gr = new TGraphErrors(file1, "%*s %*s %lg %*s %*s %*s %*s %*s %*s %lg %lg %*s");
-// TGraphErrors *gr1 = new TGraphErrors(file2, "%lg %*s %*s %lg %lg");
+TGraphErrors *gr = new TGraphErrors(file1, "%*s %*s %lg %*s %*s %lg %lg %*s %*s %*s %*s %*s");
+TGraph *gr1 = new TGraph(file2, "%*s %lg %lg");
 // TGraph *gr2 = new TGraph();
 // gr2->SetPoint(0, 50., 3.80);
 
@@ -16,26 +16,25 @@ gr->SetMarkerColor(2);
 gr->SetMarkerStyle(20);
 gr->SetMarkerSize(0.7);
 
-gr->GetXaxis()->SetRangeUser(44,92);
-// gr->GetYaxis()->SetRangeUser(-5,60);
-
-// gr1->SetMarkerColor(2);
-// gr1->SetMarkerStyle(21);
+gr1->SetMarkerColor(4);
+gr1->SetMarkerStyle(21);
+gr1->SetMarkerSize(0.7);
 
 // gr2->SetMarkerColor(6);
 // gr2->SetMarkerStyle(21);
 
-// mg->Add(gr);
-// mg->Add(gr1);
+ mg->Add(gr);
+ mg->Add(gr1, "PL");
 // mg->Add(gr2);
 
+mg->GetXaxis()->SetRangeUser(44,92);
+mg->GetYaxis()->SetRangeUser(-5,60);
 
-gr->GetXaxis()->SetTitle("Mass number (A)");
-gr->GetYaxis()->SetTitle("Average Radiative Width #LT #Gamma #GT (MeV)");
-gr->SetTitle("Mass Number (A) versus Average Radiative Width #LT #Gamma #GT (MeV) for neutron capture");
+mg->GetXaxis()->SetTitle("Mass Number (A)");
+mg->GetYaxis()->SetTitle("Average Resonance Spacing D_{0} (keV)");
 
 
-gr->Draw("AP");
+mg->Draw("AP");
 
 // auto legend = new TLegend(0.8,0.8,0.9,0.9);
 // legend->AddEntry(gr, "Sr","lp");
