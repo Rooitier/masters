6925ec3ab205125f4d2ac63a002061ced1d4fb29
Rooitier <rooitier@gmail.com>
2022-07-30 03:44:12 +0200

    Big plots

--- learning/ML_PlotMGraphRatio.cpp
+++ learning/ML_PlotMGraphRatio.cpp
@@ -1,65 +1,70 @@
-void plot(const char* file1, const char* file2, const char* file3){
+void ratioplot(const char* file1, const char* file2, const char* file3){
 
 // Graphs and canvas
 TCanvas *c1 = new TCanvas ("c1","Graph Draw Options", 500,500,600,400);
 
 TMultiGraph *mg = new TMultiGraph("mg","");
 
-TGraphErrors *gr = new TGraphErrors(file1, "%*s %lg  %lg %lg %*s %*s %*s %*s");
-// TGraph *grr = new TGraph(file1, "%lg %*s %*s %lg %*s %*s");
-TGraphErrors *gr1 = new TGraphErrors(file2, "%*s %lg %lg %lg");
-TGraphErrors *gr2 = new TGraphErrors(file3, "%*s %lg %lg %lg");
-// TGraph *gr3 = new TGraph(file4, "%lg %*s %*s %lg %*s %*s");
+// TGraphErrors *gr = new TGraphErrors(file1, "%lg %lg %lg");
+// TGraph *grr = new TGraph(file1, "%*s %lg %*s %*s %lg %*s %*s");
+TGraphErrors *gr1 = new TGraphErrors(file1, "%*s %lg %*s %*s %*s %*s %lg %lg");
+TGraphErrors  *gr2 = new TGraphErrors(file2, "%*s %lg %*s %*s %*s %*s %lg %lg");
+TGraphErrors  *gr3 = new TGraphErrors(file3, "%*s %lg %*s %*s %*s %*s %lg %lg");
 
 // Options
 
-gr->SetMarkerColor(1);
-gr->SetMarkerStyle(21);
-gr->SetMarkerSize(1);
-gr->SetLineWidth(1);
-gr->SetLineColor(1);
+// gr->SetMarkerColor(1);
+// gr->SetMarkerStyle(21);
+// gr->SetMarkerSize(0.7);
+// gr->SetLineWidth(1);
+// gr->SetLineColor(1);
 
-// grr->SetMarkerColor(4);
-// grr->SetMarkerStyle(20);
+// grr->SetMarkerColor(kBlue);
+// grr->SetMarkerStyle(24);
 
-gr1->SetMarkerColor(kRed);
-gr1->SetMarkerStyle(31);
-gr1->SetMarkerSize(1.8);
+gr1->SetMarkerColor(kBlue);
+gr1->SetMarkerStyle(24);
+gr1->SetMarkerSize(1.3);
 
 
-gr2->SetMarkerColor(kMagenta-3);
-gr2->SetMarkerSize(1.5);
+gr2->SetMarkerColor(kRed);
+gr2->SetMarkerSize(1);
 gr2->SetMarkerStyle(22);
 
-// gr3->SetMarkerColor(kGreen+3);
-// gr3->SetMarkerSize(1.5);
-// gr3->SetMarkerStyle(43); 
+gr3->SetMarkerColor(kGreen+3);
+gr3->SetMarkerSize(1.5);
+gr3->SetMarkerStyle(33); 
 
-mg->Add(gr,"p");
-// mg->Add(grr);
+// mg->Add(gr,"p");
+// mg->Add(grr,"p");
 mg->Add(gr1,"p");
 mg->Add(gr2,"p");
-// mg->Add(gr3,"p");
+mg->Add(gr3,"p");
 
 // mg->GetXaxis()->SetRangeUser(35,260);
-// mg->GetYaxis()->SetRangeUser(-2,120);
+// mg->GetYaxis()->SetRangeUser(-1000,1000);
 
 
 mg->GetXaxis()->SetTitle("Nuclear Mass (a.m.u)");
-mg->GetYaxis()->SetTitle("D_{0} (keV)");
+mg->GetYaxis()->SetTitle("D_{0}_{th}  / D_{0}_{exp} (a.u.)");
+mg->GetYaxis()->SetMaxDigits(4);
+mg->GetYaxis()->SetNdivisions(50510);
 
 //Draw
 
 mg->Draw("APL");
 
+
+
 auto legend = new TLegend(0.15,0.7,0.3,0.85);
 legend->SetBorderSize(0);
-legend->AddEntry(gr, "No Parity","lp");
-// legend->AddEntry(grr,"CTM + FGM","lp");
-legend->AddEntry(gr1,"Asymmetric","lp");
-legend->AddEntry(gr2,"Symmetric","lp");
-// legend->AddEntry(gr3,"Symmetric","lp");
+// legend->AddEntry(gr, "Experimental D_{0}","lp");
+// legend->AddEntry(grr,"CTM + FGM","p");
+legend->AddEntry(gr1,"No Parity","lp");
+legend->AddEntry(gr2,"Asymmetric","lp");
+legend->AddEntry(gr3,"Symmetric","lp");
 legend->Draw();
+
 
 TStyle *st1 = new TStyle("st1","my style");
 
